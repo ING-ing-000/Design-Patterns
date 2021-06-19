9921979e5997f71327c6f38b28719c2fa413e969
COCOHF1 <COCOHF1>
2021-06-19 13:27:16 +0800

    解释器模式

---
+++ InterprerterPattern/Client.java
@@ -0,0 +1,22 @@
+package InterprerterPattern;
+
+public class Client {
+	public static void main(String[] args) {
+		Expression isMale=getMaleExpression();
+		Expression isMarriedWoman=getMarriedWomanExpression();
+		System.out.println("John is male?"+isMale.interpret("Male John"));
+		System.out.println("Julie is a married women?"+isMarriedWoman.interpret("Married Julie"));
+	}
+	public static Expression getMaleExpression() {
+		Expression robert=new TerminalExpression("Robert");
+		Expression john=new TerminalExpression("John");
+		return new OrExpression(robert,john);
+	}
+	public static Expression getMarriedWomanExpression() {
+		Expression julie=new TerminalExpression("Julie");
+		Expression married=new TerminalExpression("Married");
+		return new AndExpression(julie,married);
+	}
+	
+	
+}
---
+++ InterprerterPattern/Expression.java
@@ -0,0 +1,5 @@
+package InterprerterPattern;
+
+public interface Expression {
+	public boolean interpret(String context);
+}
