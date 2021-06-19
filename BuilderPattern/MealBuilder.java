fdfa34a7ecd2506e690e9ee9e5241e077efbfde7
COCOHF1 <COCOHF1>
2021-06-19 13:08:19 +0800

    建造者模式

---
+++ BuilderPattern/MealBuilder.java
@@ -0,0 +1,12 @@
+package BuilderPattern;
+
+public abstract class MealBuilder {
+	protected Meal meal=new Meal();
+	
+	public abstract void buildFood();
+	public abstract void buildDrink();
+	public Meal getMeal() {
+		return meal;
+	}
+
+}
---
+++ BuilderPattern/SubMealBuilderB.java
@@ -0,0 +1,11 @@
+package BuilderPattern;
+
+public class SubMealBuilderB extends MealBuilder{
+	public void buildFood() {
+		meal.setFood("一个鸡肉卷");
+	}
+	public void buildDrink() {
+		meal.setDrink("一杯果汁");
+	}
+
+}
