60d78161135956ab036b0870138fd07f7151cc88
COCOHF1 <COCOHF1>
2021-06-19 13:29:14 +0800

    中介者模式

---
+++ MediatorPattern/AbstractChatroom.java
@@ -0,0 +1,7 @@
+package MediatorPattern;
+
+public abstract class AbstractChatroom {
+	public abstract void register(Member member);
+	public abstract void sendText(String from,String to,String message);
+	public abstract void sendImage(String from,String to,String image);
+}
